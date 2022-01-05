ccdc4e98205897e5298d60bf8e5730229f01afe6
RahmatullayevTohir <94367326+RahmatullayevTohir@users.noreply.github.com>
2022-01-05 18:06:37 +0500

    Initial commit
    landscape layout

---
+++ app/src/main/java/com/example/landscapelayout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.landscapelayout
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+    }
+    fun initViews() {
+        val button = findViewById<Button>(R.id.btn_next)
+    }
+}
